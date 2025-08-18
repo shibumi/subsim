d83bf222a73ca82b760891eb10efb1b736c4f2ea
Christian Rebischke <chris@shibumi.dev>
2025-08-19 00:38:26 +0200

    first proof of concept for single leg ekelund

--- script.js
+++ script.js
@@ -33,10 +33,69 @@ function sblCalculate() {
     document.getElementById("sbl.result").innerHTML = result + " knots.";
 }
 
+function sleCalculate() {
+    let result = 0;
+    let crso = getInt("sle.crso");
+    let spdo = getInt("sle.spdo");
+    let brg = getInt("sle.brg");
+    let crst = getInt("sle.crst");
+    let spdt = getInt("sle.spdt");
+    let brgrt = getFloat("sle.brgrt");
+    let lag = document.getElementById("sle.lag").checked
+
+    let aob = reciprocal(brg) - crst;
+    let lla = brg - crso;
+    let spdoa = Math.abs(Math.sin(degToRad(lla)) * spdo);
+    let spdta = Math.abs(Math.sin(degToRad(aob)) * spdt);
+    let spdoi = Math.abs(Math.cos(degToRad(lla)) * spdo);
+    let spdti = Math.abs(Math.cos(degToRad(aob)) * spdt);
+    let spdra = spdta - spdoa;
+
+    // If Lag LOS, we have to add SPDTA and SPDOA
+    console.log(lag)
+    if(lag) {
+        spdra = spdta + spdoa;
+    }
+    result = Math.abs(spdra / brgrt);
+    document.getElementById("aob.result").innerHTML = "AOB: " + aob;
+    document.getElementById("lla.result").innerHTML = "LLA: " + lla;
+    document.getElementById("spdoa.result").innerHTML = "SPDOA: " + spdoa + " knots.";
+    document.getElementById("spdta.result").innerHTML = "SPDTA: " + spdta + " knots.";
+    document.getElementById("spdoi.result").innerHTML = "SPDOI: " + spdoi + " knots.";
+    document.getElementById("spdti.result").innerHTML = "SPDTI: " + spdti + " knots.";
+    document.getElementById("spdra.result").innerHTML = "SPDRA: " + Math.abs(spdra) + " knots.";
+    document.getElementById("sle.result").innerHTML = "Ekelund Range: " + result + " nm.";
+}
+
 function getFloat(id) {
     return parseFloat(document.getElementById(id).value);
 }
 
 function getInt(id) {
     return parseInt(document.getElementById(id).value);
+}
+
+function reciprocal(bearing) {
+    // Ensure the bearing is a number
+    bearing = Number(bearing);
+
+    // Check for valid bearing range
+    if (isNaN(bearing) || bearing < 0 || bearing > 360) {
+      throw new Error("Invalid bearing. Please enter a value between 0 and 360.");
+    }
+
+    // Calculate reciprocal bearing
+    if (bearing < 180) {
+      return bearing + 180;
+    } else {
+      return bearing - 180;
+    }
+}
+
+function degToRad(degrees) {
+    return degrees * (Math.PI / 180);
+  }
+
+function radToDeg(rad) {
+    return rad / (Math.PI / 180);
 }
